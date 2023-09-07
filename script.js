4758a98494f21a360721f48561a553469add26eb
Mdmustakim5793 <mdmustakimd0786@gmail.com>
2023-09-07 16:36:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// console.log("mustakim warsi i am web developer");
+
+const accountId = 123456
+let accountEmail = "must@1234gmail.com"
+var accountPassword = 849842
+accountCity = "bhopal"
+let accountDate;
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountDate])
+
+/*
+preffer not to use var 
+because not supported block scope and funtional scope
+use only let..........
+*/
+
+
+"use strict"; // treat all js code as newer version 
+let name = "mustakim"
+let age = 18
+let loggedIn = false
+let state = null  // means get empty value
+
+//...........DATA TYPES............
+// number => 2 to 53
+// string => ""
+// boolean => true or false
+// bigint => 
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof null) // output object
+console.log(typeof undefined) // output undefined
